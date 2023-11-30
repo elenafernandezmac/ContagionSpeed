b10e84d217270ceeaa31e0dc0b507173157ab3c5
LucasMDoval <martindovallucas@gmail.com>
2023-11-30 11:53:07 +0100

    Update contagion.py

---
+++ contagion.py
@@ -0,0 +1,10 @@
+#1-personas que mueren por hora
+#2-velocidad de contagio
+#3-cuanto se tardará en que muera toda la poblacion
+
+
+#PARÄMETROS
+#1-poblacion total
+#2-porcentaje inicial de gente contagiada
+#3-numero de interacciones entre personas por dia
+#4-duracion del virus(infinito)
